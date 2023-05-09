52b2d5e09b27de26baf692f94dfadf06824f8135
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-09 23:05:01 +0600

    task6

---
+++ task6.js
@@ -0,0 +1,31 @@
+const studentData = [
+  {
+    class: 10,
+    details: [
+      {
+        studentId: "1",
+        gradingDetails: [{ grade: "A" }],
+      },
+      {
+        studentId: "2",
+        gradingDetails: [{ grade: "B" }],
+      },
+    ],
+  },
+  {
+    class: 11,
+    details: [
+      {
+        studentId: 3,
+        gradingDetails: [{ grade: "B" }],
+      },
+      {
+        studentId: 4,
+        gradingDetails: [{ grade: "D" }],
+      },
+    ],
+  },
+];
+
+console.log(studentData[0].details[1].gradingDetails[0].grade);
+console.log(studentData[1].details[1].gradingDetails[0].grade);
